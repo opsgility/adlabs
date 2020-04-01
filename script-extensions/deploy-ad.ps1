3b15c258e4ede8432637801672b738472e87b193
Michael Washam <michael@opsgility.com>
2020-04-01 05:10:02 -0500

    updating for win10

--- script-extensions/deploy-ad.ps1
+++ script-extensions/deploy-ad.ps1
@@ -2,13 +2,19 @@ param($domain, $password)
 
 $smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
 
+
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "UserAuthentication" -Value 0
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "SecurityLayer" -Value 0
+
+Rename-LocalUser -Name "demouser" -NewName "administrator"
+
 Install-WindowsFeature -Name "AD-Domain-Services" `
                        -IncludeManagementTools `
                        -IncludeAllSubFeature 
 
- Install-ADDSForest -DomainName $domain `
-                   -DomainMode WinThreshold `
-                   -ForestMode WinThreshold `
-                   -Force `
-                   -SafeModeAdministratorPassword $smPassword 
+Install-ADDSForest -DomainName $domain `
+                -DomainMode WinThreshold `
+                -ForestMode WinThreshold `
+                -Force `
+                -SafeModeAdministratorPassword $smPassword 
 
--- script-extensions/domain-join.ps1
+++ script-extensions/domain-join.ps1
@@ -1,5 +1,10 @@
 param($domain, $user, $password)
-Start-Sleep -Seconds 120
+#Start-Sleep -Seconds 120
+
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "UserAuthentication" -Value 0
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "SecurityLayer" -Value 0
+Rename-LocalUser -Name $user -NewName "administrator"
+
 $smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
 $user = "$domain\$user"
 $objCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($user, $smPassword)
